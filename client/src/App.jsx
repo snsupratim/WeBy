5c5d051296bd7964590a65878cade16b1769bca6
Supratim Nag <snsupratim@gmail.com>
2024-09-04 21:16:56 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { Button, Container } from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom";
+import PostPage from "./pages/PostPage";
+import UserPage from "./pages/UserPage";
+
+function App() {
+  return (
+    <Container maxW="620px">
+      <Routes>
+        <Route path="/:username" element={<UserPage />} />
+        <Route path="/:username/post/:pid" element={<PostPage />} />
+      </Routes>
+    </Container>
+  );
+}
+
+export default App;
---
+++ client/src/main.jsx
@@ -0,0 +1,40 @@
+import * as React from "react";
+import { ChakraProvider, ColorModeScript, extendTheme } from "@chakra-ui/react";
+import * as ReactDOM from "react-dom/client";
+import App from "./App";
+import { mode } from "@chakra-ui/theme-tools";
+
+const styles = {
+  global: (props) => ({
+    body: {
+      color: mode("gray.800", "whiteAlpha.900")(props),
+      bg: mode("gray.100", "#101010")(props),
+    },
+  }),
+};
+
+const config = {
+  initialColorMode: "dark",
+  useSystemColorMode: true,
+};
+
+const colors = {
+  gray: {
+    light: "#616161",
+    dark: "#1e1e1e",
+  },
+};
+
+const theme = extendTheme({ config, styles, colors });
+
+const rootElement = document.getElementById("root");
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ChakraProvider theme={theme}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
